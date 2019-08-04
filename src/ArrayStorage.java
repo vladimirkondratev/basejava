a2fdeb9868876f7a5c25899dd6e35f4260eacf76
vladimirkondratev <kondrvl@yandex.ru>
2019-08-04 10:57:13 +0900

    HW1 basejava done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = 0;
+        while (i < size && uuid.compareTo(storage[i].uuid) != 0) {
+            i++;
+        }
+        if (i < size) {
+            return storage[i];
+        } else {
+            return null;
+        }
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while (i < size && uuid.compareTo(storage[i].uuid) != 0) {
+            i++;
+        }
+        if (i < size) {
+            while (i < size - 1) {
+                storage[i] = storage[i + 1];
+                i++;
+            }
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
