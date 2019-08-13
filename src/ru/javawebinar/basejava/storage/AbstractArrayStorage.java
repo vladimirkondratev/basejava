f894f814fd9b30bb732531af3f6bbdfd5475ef5b
vladimirkondratev <kondrvl@yandex.ru>
2019-08-13 20:46:51 +0900

    HW3 modify STORAGE_LIMIT

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public abstract class AbstractArrayStorage implements Storage {
-    protected static final int STORAGE_LIMIT = 3;
+    protected static final int STORAGE_LIMIT = 10_000;
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
