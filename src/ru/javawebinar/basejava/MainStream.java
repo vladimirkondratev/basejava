ed5d166cf51dd74d250c42d3bb7d4b4aa65c65cc
vladimirkondratev <kondrvl@yandex.ru>
2019-10-14 11:36:40 +0300

    Lesson 12 HW fix

--- src/ru/javawebinar/basejava/MainStream.java
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -2,15 +2,13 @@ package ru.javawebinar.basejava;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class MainStream {
 
     public static void main(String[] args) {
-        int[] array = {9, 8};
+        int[] array = {1, 2, 3, 3, 2, 3};
         System.out.println(minValue(array));
 
         List<Integer> integers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
@@ -20,41 +18,13 @@ public class MainStream {
     }
 
     public static int minValue(int[] array) {
-        List<Integer> items = Arrays.stream(array)
+        return Arrays.stream(array)
                 .distinct()
-                .boxed()
-                .collect(Collectors.toList());
-
-        return IntStream.range(0, factorial(items.size()))
-                .mapToObj(i -> permutation(i, items))
-                .mapToInt(p -> {
-                    int result = 0;
-                    int size = p.size();
-                    for (int i = 1; i <= Math.pow(10, size - 1); i *= 10) {
-                        result += i * p.get((int) (size - Math.log10(i) - 1));
-                    }
-                    return result;
-                })
-                .min()
+                .sorted()
+                .reduce((a, b) -> b = b + a * 10)
                 .getAsInt();
     }
 
-    private static int factorial(final int num) {
-        return IntStream.rangeClosed(2, num).reduce(1, (x, y) -> x * y);
-    }
-
-    private static <T> List<T> permutation(final int count, final LinkedList<T> input, final List<T> output) {
-        if (input.isEmpty()) {
-            return output;
-        }
-        final int factorial = factorial(input.size() - 1);
-        output.add(input.remove(count / factorial));
-        return permutation(count % factorial, input, output);
-    }
-
-    private static <T> List<T> permutation(final int count, final List<T> items) {
-        return permutation(count, new LinkedList<>(items), new ArrayList<>());
-    }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = 0;
@@ -74,11 +44,13 @@ public class MainStream {
     }
 
     public static List<Integer> oddOrEvenStream(List<Integer> integers) {
-        return integers
-                .stream()
-                .filter(integer -> integers
-                        .stream()
-                        .reduce(0, Integer::sum) % 2 == integer % 2)
+        int[] sum = {0};
+        return integers.stream()
+                .map(i -> {
+                    sum[0] = sum[0] + i;
+                    return i;
+                }).sorted()
+                .filter(i -> i % 2 == sum[0] % 2)
                 .collect(Collectors.toList());
     }
 }
